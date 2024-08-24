3b8ca25e0872c106571d671580ec50e03b89421f
radik.safin <safinrraf@gmail.com>
2024-08-24 20:59:03 +0200

    Find The Closest Palindrome. Implement strStr().

---
+++ src/test/java/com/learn/leetcode/medium/daily/FindTheClosestPalindromeTest.java
@@ -0,0 +1,48 @@
+package com.learn.leetcode.medium.daily;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindTheClosestPalindromeTest {
+
+    @Test
+    void nearestPalindromic1() {
+        final var input = "123";
+        final var expected = "121";
+
+        assertEquals(expected, FindTheClosestPalindrome.nearestPalindromic(input));
+    }
+
+    @Test
+    void nearestPalindromic2() {
+        final var input = "1";
+        final var expected = "0";
+
+        assertEquals(expected, FindTheClosestPalindrome.nearestPalindromic(input));
+    }
+
+    @Test
+    void nearestPalindromic3() {
+        final var input = "11";
+        final var expected = "9";
+
+        assertEquals(expected, FindTheClosestPalindrome.nearestPalindromic(input));
+    }
+
+    @Test
+    void nearestPalindromic4() {
+        final var input = "3";
+        final var expected = "2";
+
+        assertEquals(expected, FindTheClosestPalindrome.nearestPalindromic(input));
+    }
+
+    @Test
+    void nearestPalindromic5() {
+        final var input = "12";
+        final var expected = "11";
+
+        assertEquals(expected, FindTheClosestPalindrome.nearestPalindromic(input));
+    }
+}
