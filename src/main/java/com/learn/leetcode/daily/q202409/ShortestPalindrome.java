3cc3808dcc5f79f39756bf8d8b5a98cd1eb673a7
radik.safin <safinrraf@gmail.com>
2024-09-21 21:34:53 +0200

    ShortestPalindrome. LexicographicalNumbers.

---
+++ src/main/java/com/learn/leetcode/daily/q202409/ShortestPalindrome.java
@@ -0,0 +1,33 @@
+package com.learn.leetcode.daily.q202409;
+
+/**
+ * <a href="https://leetcode.com/problems/shortest-palindrome/description/?envType=daily-question&envId=2024-09-20">Task</a>
+ * <br><a href="https://leetcode.com/problems/shortest-palindrome/solutions/5810352/beats-100-beginner-friendly-python3-java-c-c-ruby-go-javascript/?envType=daily-question&envId=2024-09-20">Explanation</a>
+ */
+public final class ShortestPalindrome {
+    public static String shortestPalindrome(String s) {
+        int count = kmp(new StringBuilder(s).reverse().toString(), s);
+        return new StringBuilder(s.substring(count)).reverse().toString() + s;
+    }
+
+    private static int kmp(String txt, String patt) {
+        String newString = patt + '#' + txt;
+        int[] pi = new int[newString.length()];
+        int i = 1, k = 0;
+        while (i < newString.length()) {
+            if (newString.charAt(i) == newString.charAt(k)) {
+                k++;
+                pi[i] = k;
+                i++;
+            } else {
+                if (k > 0) {
+                    k = pi[k - 1];
+                } else {
+                    pi[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return pi[newString.length() - 1];
+    }
+}
---
+++ src/test/java/com/learn/leetcode/daily/q202409/LexicographicalNumbersTest.java
@@ -0,0 +1,28 @@
+package com.learn.leetcode.daily.q202409;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LexicographicalNumbersTest {
+
+    @Test
+    void lexicalOrder1() {
+        final var n = 13;
+        final var expected = List.of(1,10,11,12,13,2,3,4,5,6,7,8,9);
+        final var result = LexicographicalNumbers.lexicalOrder(n);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void lexicalOrder2() {
+        final var n = 2;
+        final var expected = List.of(1,2);
+        final var result = LexicographicalNumbers.lexicalOrder(n);
+
+        assertEquals(expected, result);
+    }
+}
