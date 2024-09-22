0674163b2c1f18e8b6c11e7c4f3e4cbad1f02c3d
radik.safin <safinrraf@gmail.com>
2024-09-22 17:36:15 +0200

    KthSmallestInLexicographicalOrder.

---
+++ src/test/java/com/learn/leetcode/daily/q202409/KthSmallestInLexicographicalOrderTest.java
@@ -0,0 +1,50 @@
+package com.learn.leetcode.daily.q202409;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KthSmallestInLexicographicalOrderTest {
+
+    @Test
+    void findKthNumber1() {
+        final var n = 13;
+        final var k = 2;
+        final var expected = 10;
+        final var result = KthSmallestInLexicographicalOrder.findKthNumber(n, k);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findKthNumber2() {
+        final var n = 804289384;
+        final var k = 42641503;
+        final var expected = 138377349;
+        final var result = KthSmallestInLexicographicalOrder.findKthNumber(n, k);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findKthNumber3() {
+        final var n = 1;
+        final var k = 1;
+        final var expected = 1;
+        final var result = KthSmallestInLexicographicalOrder.findKthNumber(n, k);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findKthNumber4() {
+        final var n = 681692778;
+        final var k = 351251360;
+        final var expected = 416126219;
+        final var result = KthSmallestInLexicographicalOrder.findKthNumber(n, k);
+
+        assertEquals(expected, result);
+    }
+}
