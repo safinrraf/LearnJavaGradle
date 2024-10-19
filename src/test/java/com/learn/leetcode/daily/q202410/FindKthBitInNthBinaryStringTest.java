6dc5cd94936dfbec9a468a58b94612d58fb6b474
radik.safin <safinrraf@gmail.com>
2024-10-19 20:30:58 +0200

    FindKthBitInNthBinaryString.

---
+++ src/test/java/com/learn/leetcode/daily/q202410/FindKthBitInNthBinaryStringTest.java
@@ -0,0 +1,44 @@
+package com.learn.leetcode.daily.q202410;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <a href="">Task</a>
+ * <br><a href="">Explanation</a>
+ * <ul>Topics
+ * <li> {@code ..., ..., ...}
+ * </ul>
+ * <ul>Hints
+ * <li> {@code ...}
+ * </ul>
+ * <ul><b>Constraints</b>:
+ * <li> {@code 1 ...}
+ * </ul>
+ * <br>@author radik.safin
+ * <br>@since 1.0.0
+ * <br> 19.10.24 20:15
+ */
+class FindKthBitInNthBinaryStringTest {
+
+    @Test
+    void findKthBit1() {
+        final var n = 3;
+        final var k = 1;
+        final var expected = '0';
+        final var result = FindKthBitInNthBinaryString.findKthBit(n, k);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findKthBit2() {
+        final var n = 4;
+        final var k = 11;
+        final var expected = '1';
+        final var result = FindKthBitInNthBinaryString.findKthBit(n, k);
+
+        assertEquals(expected, result);
+    }
+}
