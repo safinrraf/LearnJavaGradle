24d907c60c8c07ae8ca373971c5ed2d702235d4d
radik.safin <safinrraf@gmail.com>
2024-11-05 09:00:56 +0100

    MinimumNumberOfChangesToMakeBinaryStringBeautiful.

---
+++ src/test/java/com/learn/leetcode/daily/u202411/medium/MinimumNumberOfChangesToMakeBinaryStringBeautifulTest.java
@@ -0,0 +1,45 @@
+package com.learn.leetcode.daily.u202411.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MinimumNumberOfChangesToMakeBinaryStringBeautifulTest {
+
+    @Test
+    void minChanges1() {
+        var input = "1001";
+        var expected = 2;
+        var result = MinimumNumberOfChangesToMakeBinaryStringBeautiful.minChanges(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void minChanges2() {
+        var input = "10";
+        var expected = 1;
+        var result = MinimumNumberOfChangesToMakeBinaryStringBeautiful.minChanges(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void minChanges3() {
+        var input = "0000";
+        var expected = 0;
+        var result = MinimumNumberOfChangesToMakeBinaryStringBeautiful.minChanges(input);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void minChanges4() {
+        var input = "10010100000111001101011000000010110011";
+        var expected = 8;
+        var result = MinimumNumberOfChangesToMakeBinaryStringBeautiful.minChanges(input);
+
+        assertEquals(expected, result);
+    }
+}
