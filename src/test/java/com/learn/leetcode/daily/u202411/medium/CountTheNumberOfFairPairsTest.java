34e11553f2a191754726bf122b0dbfaa59d612da
radik.safin <safinrraf@gmail.com>
2024-11-13 16:46:30 +0100

    CountTheNumberOfFairPairs.

---
+++ src/test/java/com/learn/leetcode/daily/u202411/medium/CountTheNumberOfFairPairsTest.java
@@ -0,0 +1,31 @@
+package com.learn.leetcode.daily.u202411.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CountTheNumberOfFairPairsTest {
+
+    @Test
+    void countFairPairs1() {
+        final int[] nums = {0,1,7,4,4,5};
+        final var lower = 3;
+        final var upper = 6;
+        final var expected = 6L;
+        final var result = CountTheNumberOfFairPairs.countFairPairs(nums, lower, upper);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void countFairPairs2() {
+        final int[] nums = {1,7,9,2,5};
+        final var lower = 11;
+        final var upper = 11;
+        final var expected = 1L;
+        final var result = CountTheNumberOfFairPairs.countFairPairs(nums, lower, upper);
+
+        assertEquals(expected, result);
+    }
+}
