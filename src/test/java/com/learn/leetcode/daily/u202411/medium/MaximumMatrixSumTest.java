666d0822380362fdc355037fb9ac3ebfcea7ea08
radik.safin <safinrraf@gmail.com>
2024-11-24 16:40:26 +0100

    MaximumMatrixSum.

---
+++ src/test/java/com/learn/leetcode/daily/u202411/medium/MaximumMatrixSumTest.java
@@ -0,0 +1,33 @@
+package com.learn.leetcode.daily.u202411.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumMatrixSumTest {
+
+    @Test
+    void maxMatrixSum1() {
+        final int[][] matrix = {
+                {1,-1},
+                {-1,1}
+        };
+        final long expected = 4L;
+        final long result = MaximumMatrixSum.maxMatrixSum(matrix);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void maxMatrixSum2() {
+        final int[][] matrix = {
+                {1,2,3},
+                {-1,-2,-3},
+                {1,2,3}
+        };
+        final long expected = 16L;
+        final long result = MaximumMatrixSum.maxMatrixSum(matrix);
+
+        assertEquals(expected, result);
+    }
+}
