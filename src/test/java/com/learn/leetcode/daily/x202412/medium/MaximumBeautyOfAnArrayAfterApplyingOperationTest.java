2674c54882b528880e310d7add30eab3d187bd16
radik.safin <safinrraf@gmail.com>
2024-12-11 20:33:43 +0100

    MaximumBeautyOfAnArrayAfterApplyingOperation.

---
+++ src/test/java/com/learn/leetcode/daily/x202412/medium/MaximumBeautyOfAnArrayAfterApplyingOperationTest.java
@@ -0,0 +1,28 @@
+package com.learn.leetcode.daily.x202412.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumBeautyOfAnArrayAfterApplyingOperationTest {
+
+    @Test
+    void maximumBeauty1() {
+        final int[] nums = {4,6,1,2};
+        final int k = 2;
+        final int expected = 3;
+        final int result = MaximumBeautyOfAnArrayAfterApplyingOperation.maximumBeauty(nums, k);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void maximumBeauty2() {
+        final int[] nums = {1,1,1,1};
+        final int k = 10;
+        final int expected = 4;
+        final int result = MaximumBeautyOfAnArrayAfterApplyingOperation.maximumBeauty(nums, k);
+
+        assertEquals(expected, result);
+    }
+}
