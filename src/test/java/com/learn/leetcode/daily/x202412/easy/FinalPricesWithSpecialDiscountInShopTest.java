6cec15c5bed61fce9eb46b632c564e0579e99ce1
radik.safin <safinrraf@gmail.com>
2024-12-18 21:15:09 +0100

    FinalPricesWithSpecialDiscountInShop.

---
+++ src/test/java/com/learn/leetcode/daily/x202412/easy/FinalPricesWithSpecialDiscountInShopTest.java
@@ -0,0 +1,62 @@
+package com.learn.leetcode.daily.x202412.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinalPricesWithSpecialDiscountInShopTest {
+
+    @Test
+    void finalPrices1() {
+        final int[] prices = {8,4,6,2,3};
+        final int[] expected = {4,2,4,2,3};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices(prices);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void finalPrices2() {
+        final int[] prices = {1,2,3,4,5};
+        final int[] expected = {1,2,3,4,5};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices(prices);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void finalPrices3() {
+        final int[] prices = {10,1,1,6};
+        final int[] expected = {9,0,1,6};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices(prices);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void finalPrices4() {
+        final int[] prices = {8,4,6,2,3};
+        final int[] expected = {4,2,4,2,3};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices2(prices);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void finalPrices5() {
+        final int[] prices = {1,2,3,4,5};
+        final int[] expected = {1,2,3,4,5};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices2(prices);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void finalPrices6() {
+        final int[] prices = {10,1,1,6};
+        final int[] expected = {9,0,1,6};
+        final int[] result = FinalPricesWithSpecialDiscountInShop.finalPrices2(prices);
+
+        assertArrayEquals(expected, result);
+    }
+}
